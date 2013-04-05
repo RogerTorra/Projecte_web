1a084f33ce39f7ebd9779a5bc55d72e0c3417d2b
ROGER <elrogertorra@gmail.com>
2013-04-05 10:50:16 +0200

    Comit 5/04/2013

--- web/gestorFestes/models.py
+++ web/gestorFestes/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 # Create your models here.
 
-class Festa(models.Model):
-	titol = models.CharField(max_length=25, unique=True)
-	descripcio = models.TextField(max_length=200)
-	imatge = models.ImageField(upload_to='imatgesFesta', verbose_name='Imatge')
-	data = models.DateTimeField()
-
+class Ciutat(models.Model):
+	nom = models.CharField(max_length=25, unique=True)
+	provincia = models.CharField(max_length=25)
+	cp = models.IntegerField(verbose_name='Codi Postal')
 
 class Local(models.Model):
 	nom = models.CharField(max_length=25, unique=True)
@@ -16,10 +14,14 @@ class Local(models.Model):
 	carrer = models.TextField(max_length=50)
 	telf = models.IntegerField(verbose_name='Telefon')
 	email = models.EmailField(verbose_name='Email de contacte', max_length="30")
-	
+	ciutat = models.ForeignKey(Ciutat)
+	def __unicode__(self):
+		return self.nom
 
-class Ciutat(models.Model):
-	nom = models.CharField(max_length=25, unique=True)
-	provincia = models.CharField(max_length=25)
-	cp = models.IntegerField(verbose_name='Codi Postal')
-	
+
+class Festa(models.Model):
+	titol = models.CharField(max_length=25, unique=True)
+	descripcio = models.TextField(max_length=200)
+	imatge = models.ImageField(upload_to='imatgesFesta', verbose_name='Imatge')
+	data = models.DateTimeField()
+	local = models.ForeignKey(Local)
