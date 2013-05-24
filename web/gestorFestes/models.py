66e6a9020e5318d69bc2e14d9a36ae78ee398236
ROGER <elrogertorra@gmail.com>
2013-05-24 09:40:06 +0200

    auto super user created

--- web/gestorFestes/models.py
+++ web/gestorFestes/models.py
@@ -2,6 +2,37 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.management import create_superuser
+from django.db.models import signals
+from django.conf import settings
+
+# Prevent interactive question about wanting a superuser created.  (This code
+# has to go in this otherwise empty "models" module so that it gets processed by
+# the "syncdb" command during database creation.)
+signals.post_syncdb.disconnect(
+    create_superuser,
+    sender=auth_models,
+    dispatch_uid='django.contrib.auth.management.create_superuser')
+
+
+# Create our own test user automatically.
+
+def create_testuser(app, created_models, verbosity, **kwargs):
+  if not settings.DEBUG:
+    return
+  try:
+    auth_models.User.objects.get(username='admin')
+  except auth_models.User.DoesNotExist:
+    print '*' * 80
+    print 'Creating Admin user -- login: admin, password: admin'
+    print '*' * 80
+    assert auth_models.User.objects.create_superuser('admin', 'x@x.com', 'admin')
+  else:
+    print 'Test user already exists.'
+
+signals.post_syncdb.connect(create_testuser,
+    sender=auth_models, dispatch_uid='common.models.create_testuser')
 
 # Create your models here.
 class Ciutat(models.Model):
